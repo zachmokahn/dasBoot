218aabd8cf03b48d5d58727cb28a0c920e93ad2d
Zachary Davy <zacharyjdavy@gmail.com>
2014-03-27 07:32:37 -0500

    forgot to close the socket

--- src/dasBoot/Sockets/ServerSocket/iServerSocket.java
+++ src/dasBoot/Sockets/ServerSocket/iServerSocket.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import dasBoot.Sockets.Socket.iSocket;
 
 public interface iServerSocket {
+  public void close() throws IOException;
   public boolean isOpen();
   public iSocket listen() throws IOException;
 }
